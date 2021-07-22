5225087d3902babcdfc4d58f807bc71d8598d402
Henric Andersson <henric@sensenet.nu>
2021-07-23 00:30:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Bridges two serial devices on a raspberry pi.
+
+Very simple, if it finds 3 ttys on boot, it will assume that the two which aren't the built-in serial port shall be bridged.
+If it only finds two ttys, it assumes those should be bridged.
+Any other amount (more or less) will cause the software to exit
+
+To install, you need python3-serial (ie, sudo apt install python3-serial)
+
---
+++ bridge.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import subprocess
+import re
+import logging
+import serial
+import select
+
+def find_ports():
+  dmesg = subprocess.run(['dmesg'], stdout=subprocess.PIPE)
+
+  s = dmesg.stdout.decode('utf-8')
+  r = re.findall(r'tty[A-Z]{3}[0-9]', s)
+
+  for serial in r:
+    print(f'Found {serial} port')
+
+  if len(r) < 2:
+    logging.error('Too few ports, cannot bridge')
+    sys.exit(255)
+  if len(r) > 3:
+    logging.error('Too many ports, do not know what to bridge')
+    sys.exit(255)
+  if len(r) == 2 and 'ttyAMA0' not in r:
+    logging.error('Native UART not found (looking for ttyAMA0)')
+    sys.exit(255)
+
+  if len(r) == 3: # Standard bridging
+    r.remove('ttyAMA0')
+
+  return r
+
+def open_port(port):
+  p = serial.Serial(f'/dev/{port}', baudrate=19200)
+  p.reset_input_buffer()
+  p.reset_output_buffer()
+  return p
+
+def transfer_data(a, b):
+  data = a.read(a.in_waiting)
+  b.write(data)
+  return data
+
+r = find_ports()
+
+srcport = r[0]
+dstport = r[1]
+
+print(f'Bridging between {srcport} and {dstport}')
+
+src = open_port(srcport)
+dst = open_port(dstport)
+
+ports = [src, dst]
+
+while True:
+  readable, writeable, exceptional = select.select(ports, [], ports)
+  if src in readable:
+    c = transfer_data(src, dst)
+    print('SRC->DST: ' + repr(c))
+  if dst in readable:
+    c = transfer_data(dst, src)
+    print('SRC<-DST: ' + repr(c))
+  if len(exceptional):
+    print('Error detected on serial communication')
+    break
