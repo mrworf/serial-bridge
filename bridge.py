015090b4dfb14861466a60b00f8363eae6d1c117
Henric Andersson <henric@sensenet.nu>
2021-07-24 04:10:52 +0100

    Handle duplicate ports and exceptions
    - Duplicate ttys may show from dmesg, filter them
    - Handle serial exceptions first, try reopening failing port

--- bridge.py
+++ bridge.py
@@ -5,12 +5,15 @@ import re
 import logging
 import serial
 import select
+import sys
 
 def find_ports():
   dmesg = subprocess.run(['dmesg'], stdout=subprocess.PIPE)
 
   s = dmesg.stdout.decode('utf-8')
   r = re.findall(r'tty[A-Z]{3}[0-9]', s)
+
+  r = list(set(r))
 
   for serial in r:
     print(f'Found {serial} port')
@@ -55,12 +58,28 @@ ports = [src, dst]
 
 while True:
   readable, writeable, exceptional = select.select(ports, [], ports)
+  if len(exceptional):
+    print('Error detected on serial communication')
+    if src in exceptional:
+      logging.info(f'Reopening {srcport}')
+      try:
+        src.close()
+      except:
+        logging.exception('Unable to close src')
+      src = open_port(srcport)
+    if dst in exceptional:
+      logging.info(f'Reopening {dstport}')
+      try:
+        dst.close()
+      except:
+        logging.exception('Unable to close dst')
+      dst = open_port(dstport)
+    ports = [src, dst]
+    logging.info('Resuming bridging')
+    continue
   if src in readable:
     c = transfer_data(src, dst)
     print('SRC->DST: ' + repr(c))
   if dst in readable:
     c = transfer_data(dst, src)
     print('SRC<-DST: ' + repr(c))
-  if len(exceptional):
-    print('Error detected on serial communication')
-    break
